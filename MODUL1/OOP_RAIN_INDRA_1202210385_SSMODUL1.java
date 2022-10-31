324a890ef15b69ab621a4d596f5e0a8281aaecfc
Indrarachman12 <116269545+Indrarachman12@users.noreply.github.com>
2022-10-31 12:05:40 +0700

    Add files via upload

---
+++ MODUL1/OOP_RAIN_INDRA_1202210385_SSMODUL1.java
@@ -0,0 +1,40 @@
+public class Restaurant {
+    public String database;
+    public String menu;
+    public String server;
+    public String user;
+}
+
+public class database {  
+    String insertmenu;
+    String showmenu;
+    String searchmenu;
+    
+    public void setInsert (String insertmenu){
+        this.insertmenu = "Masukkan Pilihan : ";
+        this.showmenu = """
+                1. Pizza = Rp. 30000
+                2. Mojito = Rp. 15000
+                3. Desser Box = Rp. 20000
+                """;
+        this.searchmenu = """
+                1. Menu
+                2. Pilih Menu
+                3. Keluar
+                """;
+
+public class user {
+    String name;
+    Integer nohp;
+
+    public void setName(String name){
+        this.name = "Indra_SI4506";
+    }
+    public void setNohp (Integer nohp){
+        this.nohp = 081382913812;
+    }
+
+}
+
+    }
+}
