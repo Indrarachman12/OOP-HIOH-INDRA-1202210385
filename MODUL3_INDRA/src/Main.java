4ca88b235cb381486ab80853b403ae54803f1df0
Indrarachman12 <116269545+Indrarachman12@users.noreply.github.com>
2022-12-26 12:08:07 +0700

    Add files via upload

---
+++ MODUL3_INDRA/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        boolean repeat = true;
+        Calculation calculation = new Calculation();
+
+        do {
+            try {
+              System.out.println("Menu:");
+              System.out.println("1. Square");
+              System.out.println("2. Circle");
+              System.out.println("3. Trapezoid");
+              System.out.println("0. exit");
+              System.out.print("pilihan: ");
+            }
+        }
+    }
+}
